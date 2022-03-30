d6f8d0016af1140ee37fb29798f79e9d3f4b2170
ratnasaridewi29 <80752516+ratnasaridewi29@users.noreply.github.com>
2022-03-30 12:05:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD 2020 PHP & MySQL + Bootstrap 4</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="container"></div>
+<h1 class="text-center">COBA CRUD</h1>
+<h2 class="text-center">yo bisa yo</h2>
+
+<!-- Awal Card Form -->
+<div class="card">
+  <div class="card-header bg-primary text-putih">
+    CRUD
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Membuat CRUD</h5>
+    <p class="card-text">Created, Read, Update, Delete</p>
+    <a href="#" class="btn btn-primary">GO</a>
+  </div>
+</div>
+<!-- Akhir Card Form -->
+
+<script type="text/javascript" src="js/bootstrap.min.css"></script>
+</body>
+</html>
